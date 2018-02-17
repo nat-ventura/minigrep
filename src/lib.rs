a8ba646b44b29427b90ffbf527f21cce0c1d38ac
Natalie Ventura Villasana <nat@storj.io>
2018-02-17 11:45:59 -0700

    creates failing test for not yet existing search fn

--- src/lib.rs
+++ src/lib.rs
@@ -32,3 +32,22 @@ pub fn run(config: Config) -> Result<(), Box<Error>> {
     // that is, doesn't return a value we need
     Ok(())
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+        Rust:
+        safe, fast, productive.
+        Pick three.";
+
+        assert_eq!(
+            vec!["safe, fast, productive."],
+            search(query, contents)
+            );
+    }
+}
