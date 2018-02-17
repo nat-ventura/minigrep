9a07c52755705c709123a07763627bb6b6b9eaa6
Natalie Ventura Villasana <nat@storj.io>
2018-02-16 21:11:31 -0700

    returns errors from the run function

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,37 @@
 use std::env;
 use std::fs::File;
 use std::io::prelude::*;
+use std::process;
+use std::error::Error;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
-
-    let (query, filename) = parse_config(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
     // let query = &args[1];
     // let filename = &args[2];
+    
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
 
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
+    run(config);
+}
 
-    let mut f = File::open(filename).expect("file not found");
+fn run(config: Config) -> Result<(), Box<Error>> {
+    let mut f = File::open(config.filename)?;
 
     let mut contents = String::new();
-    f.read_to_string(&mut contents)
-        .expect("something went wrong reading the file");
+    f.read_to_string(&mut contents)?;
 
     println!("With text:\n{}", contents);
+
+    // indicates we're calling run() for its side effects only
+    // that is, it doesn't return a value we need
+    Ok(())
 }
 
 struct Config {
@@ -30,10 +40,14 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            panic!("not enough arguments");
+        }
+
         let query = args[1].clone();
         let filename = args[2].clone();
         
-        Config { query, filename }
+        Ok(Config { query, filename })
     }
 }
