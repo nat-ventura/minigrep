de438b7e4ffe6780c552558ef7229f5482609e86
Natalie Ventura Villasana <nat@storj.io>
2018-03-02 15:15:44 -0500

    changes for loops in search functions into iterators

--- src/lib.rs
+++ src/lib.rs
@@ -55,29 +55,17 @@ pub fn run(config: Config) -> Result<(), Box<Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-
-    results
+    contents.lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     let query = query.to_lowercase();
-    let mut results = Vec::new();
 
-    for line in contents.lines() {
-        // .to_lowercase() creates new data
-        if line.to_lowercase().contains(&query) {
-            results.push(line);
-        }
-    }
-
-    results
+    contents.lines()
+        .filter(|line| line.to_lowercase().contains(&query))
+        .collect()
 }
 
 #[cfg(test)]
@@ -88,6 +76,7 @@ mod test {
     fn one_result() {
         let query = "duct";
         let contents = "\
+
 Rust:
 safe, fast, productive.
 Pick three.
